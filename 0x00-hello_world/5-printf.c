736af4d07b310f52b7ed3f19f2998ba4e1317aac
benkalyx <benkalyx@gmail.com>
2023-08-10 18:46:35 +0000

    Hello puts function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,17 +3,16 @@
  * This script shows how the printf function works. 
 */
 
-int main (void) 
+int main(void) 
 {
 	/** 
 	  *This function defines the statement to print in the parentesis
 	  *which either returns 0 if true or 1 if otherwise.
 	  */
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0); /**
 		      * The program will return 0 by printing the state-
 		      * ment in the parenthesis or return 1 if the state-
 		      * ment is otherwise
 		      */
-
 }
