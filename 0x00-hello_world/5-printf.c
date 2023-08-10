fd583918cdbe3f17476a213c586cf97c1b34e113
benkalyx <benkalyx@gmail.com>
2023-08-10 17:33:13 +0000

    hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
+/**
+  This script shows how the printf function works. 
+*/
+
 int main(void) 
 {
-	/** 
-	  *This is the function to print a script
-	*/
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
