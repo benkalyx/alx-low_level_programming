5e038d10c9287e9d10eb90baed3697bacbde4258
benkalyx <benkalyx@gmail.com>
2023-08-10 17:28:29 +0000

    hello print function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
-int main(){
+int main(void) 
+{
+	/** 
+	  *This is the function to print a script
+	*/
 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
