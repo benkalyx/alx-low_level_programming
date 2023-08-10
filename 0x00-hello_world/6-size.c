f5590513a35f54611b87885afbf82f49d7c3f9f5
benkalyx <benkalyx@gmail.com>
2023-08-10 19:41:45 +0000

    Size of different types on computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int y;
 float z;
 long int yy;
 long long int yyy;
-printf("Size of a char: %lu is byte(s)\n", sizeof(char));
-printf("Size of an int: %lu is byte(s)\n", sizeof(int));
-printf("Size of a long int: %lu is byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %lu is byte(s)\n", sizeof(long long int));
-printf("Size of a float: %lu is byte(s)\n", sizeof(float));
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
 return (0);
 }
