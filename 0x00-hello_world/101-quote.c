129054beaca476bacf8d6f46b1bf8010b5487b48
benkalyx <benkalyx@gmail.com>
2023-08-10 20:10:16 +0000

    quote in c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<unistdio.h>
+/**
+* main - This is the program that prints a line and output error
+* Return: 1 (Success)
+*/
+int main(void)
+{
+fput(" and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
