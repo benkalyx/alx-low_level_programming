7a01601870b7760c10834e1d55a7cc36da9ec04d
benkalyx <benkalyx@gmail.com>
2023-08-11 22:50:02 +0000

    combination of 3 numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+/**
+* main - A program to prints possible different combination of three digits
+* Return: The return value of the program is 0
+*/
+int main(void)
+{
+	int i, j, k;
+
+	for (i = '0'; i < '9'; i++)
+	{
+	for (j = i + 1; j <= '9'; j++)
+	{
+	for (k = j + 1; k <= '9'; k++)
+	{
+	if ((j != i) != k)
+	{
+	putchar(i);
+	putchar(j);
+	putchar(k);
+	if (i == '7' && j == '8')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	return (0);
+}
