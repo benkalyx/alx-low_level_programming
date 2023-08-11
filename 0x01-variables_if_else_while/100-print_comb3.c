67b0428cb824c017444a36807fdf7d49996db53c
benkalyx <benkalyx@gmail.com>
2023-08-11 22:24:55 +0000

    Combination of different numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/**
+* main - A program to print all different possible combinations of two digits
+* Return: The return value is 0
+*/
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = '0'; i < '9'; i++)
+	{
+	for (j = i + 1; j <= '9'; j++)
+	{
+		if (i != j)
+		{
+			putchar(i);
+			putchar(j);
+		if (i =='8' && j == '9')
+		continue;
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
