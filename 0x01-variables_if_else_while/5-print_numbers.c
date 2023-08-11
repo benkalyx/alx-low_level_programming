829615379f9f75e1afd79cef4bb1742ad45ae8a3
benkalyx <benkalyx@gmail.com>
2023-08-11 17:44:06 +0000

    Numbers in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+* main - A program to print all single digit numbers of base 10 starting from0
+* Return: The return value is 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+		printf("\n");
+	return (0);
+}
