e9089ce0adab92c68b61c3af62bb109823bd4f57
benkalyx <benkalyx@gmail.com>
2023-08-11 17:10:07 +0000

    Print alphabet excluding e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+* main - A program to print alphabets excluding some characters
+* Return: 0 as success
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		if
+		(i == 'e' || i == 'q')
+		{
+			i++;
+			continue;
+		}
+		else
+			putchar(i);
+			i++;
+		putchar('\n');
+	return (0);
+}
