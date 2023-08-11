97fa8e48a2d6c63576b8e3abf96e818ee93949a1
benkalyx <benkalyx@gmail.com>
2023-08-11 23:40:46 +0000

    Comb5

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,25 +5,21 @@
 */
 int main(void)
 {
-	int i, j, k;
+	int i, j;
 
-	for (i = '0'; i < '9'; i++)
+	for (i = 0; i <= 98; i++)
 	{
-	for (j = i + 1; j <= '9'; j++)
+	for (j = i + 1; j <= 99; j++)
 	{
-	for (k = j + 1; k <= '9'; k++)
-	{
-	if ((j != i) != k)
-	{
-	putchar(i);
-	putchar(j);
-	putchar(k);
-	if (i == '7' && j == '8')
+	putchar((i / 10) + '0');
+	putchar((i % 10) + '0');
+	putchar(' ');
+	putchar(j / 10) + '0');
+	putchar(j / 10) + '0');
+	if (i == 98 && j == 99)
 	continue;
 	putchar(',');
 	putchar(' ');
-	}
-	}
 	}
 	}
 	putchar('\n');
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+* main - A program to print all possible combinations of two two digit numbers
+* Return: The return value is 0
+*/
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 98; i++)
+	{
+	for (j = i + 0; j <= 99; j++)
+	{
+	putchar((i / 10) + '0');
+	putchar((i % 10) + '0');
+	putchar(',');
+	putchar((j / 10) + '0');
+	putchar((j % 10) + '0');
+	if (i == 98 && j == 99)
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
