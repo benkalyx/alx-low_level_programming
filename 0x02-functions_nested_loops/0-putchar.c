a2a7f34070c6fda1cc863100f029d066dbce7a44
benkalyx <benkalyx@gmail.com>
2023-08-15 16:47:40 +0000

    Print _putchar as user defined function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* main - A program to print _putchar for a user defined header and functions
+* Description: 'A function to print _putchar to stdout'
+* Return: The program returns 0 if successful, and 1 if error occurs
+*/
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,5 @@
+# Functions and Nested Loop
+# This is a part of **Alx_School** program. The task is to come up with solutions to meet the learning objectives at ALX_School.
+
+Tasks          			|	Description
+0._putchar			| This tasks describes the program to print _putchar followed by a newline
