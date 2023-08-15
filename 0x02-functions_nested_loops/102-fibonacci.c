2f9a83a57cf0a4148f5ff5194d77337f16ca3990
benkalyx <benkalyx@gmail.com>
2023-08-15 23:59:26 +0000

    Program to print 50 fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+* main - A program that prints 50 fibonacci numbers
+*
+* Return: 0 if successful, otherwise return 1
+*/
+int main(void)
+{
+	int count;
+	unsigned long f_1 = 0, f_2 = 1, sum;
+
+	for (count = 0; count < 50; count++)
+	{
+		sum = f_1 + f_2;
+		printf("%lu", sum);
+
+		f_1 = f_2;
+		f_2 = sum;
+		if
+		(count == 49)
+			printf("\n");
+		else
+			printf(",");
+	}
+	return (0);
+}
