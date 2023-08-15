fdd483203f443f9810119cc8d9ee4f20465f800b
benkalyx <benkalyx@gmail.com>
2023-08-15 18:57:38 +0000

    Check for lowercase character using ASCII value

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _islower - A program to check for lowercase character
+*
+* @c: is ASCII value
+*
+* Return: return 1 if the character is lower and 0 if otherwise
+*/
+int _islower(int c)
+{
+	if
+	(c >=  97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
