de0ea5abd5f807a54ba5e713cebf5099e8887781
benkalyx <benkalyx@gmail.com>
2023-08-17 13:50:18 +0000

    Checking for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _isupper - A function to check for uppercase character
+*
+* @c: a character to search for which is passed as argument
+*
+* Return: returns 1 if success or 0 otherwise
+*/
+int _isupper(int c)
+{
+	if
+	(c <= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,6 @@
+## The Readme file for more functiona nested loop ###
+Each of this task is a requirement for the ALX software engineering program.
+To be updated
+**********************************************************************
+
+task 1: A function that checks for uppercase character.
