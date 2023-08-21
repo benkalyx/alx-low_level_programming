18d16ca44bc04a00d913fc83d78310eccd317a17
benkalyx <benkalyx@gmail.com>
2023-08-21 22:02:52 +0000

    Converts strings to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+* _atoi - A function to convert a string to an integer
+* @s: input string
+* Return: Always 0
+*/
+int _atoi(char *s)
+{
+	int a, b, c, d, e, digit;
+
+	a = 0;
+	b = 0;
+	c = 0;
+	d = 0;
+	e = 0;
+	digit = 0;
+
+	while (s[d] != '\0')
+		b++;
+	while (a < d && e == 0)
+	{
+		if (s[a] == '-')
+			++b;
+		if (s[a] >= '0' && s[a] <= '9')
+		{
+			digit = s[a] - '0';
+			if (b % 2)
+				digit = -digit;
+			c = c * 10 + digit;
+			e = 1;
+			if (s[a + 1] < '0' || s[a + 1] > '9')
+				break;
+			e = 0;
+		}
+		a++;
+	}
+	if (e == 0)
+		return (0);
+	return (c);
+}
