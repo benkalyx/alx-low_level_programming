9d2d9f1a6f2b517a19b1c781b26bdcc02c7abd9e
benkalyx <benkalyx@gmail.com>
2023-08-21 18:37:02 +0000

    Takes a pointer to an int as paramter

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* reset_to_98 - A function that takes a pointer to an int as parameter
+*
+*@*n: A pointer passed to the function as argument
+*
+* Return: 0 as success
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+ A readme file for alx pointer and array task
+ ************************************************
+ The file will be updated:
